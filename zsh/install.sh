c50362bb71e8e0e2d49363b6f85f4cc4bb38147b
Rashiq <rashiq.z@gmail.com>
2019-01-27 13:40:21 +0100

    Add zsh prompt theme

---
+++ zsh/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ZSHRC_FILE=~/.zshrc
+ln -sf ~/.dotfiles/zsh/.zshrc $ZSHRC_FILE
+
+ZSH_THEME_DIR=~/.oh-my-zsh/custom/themes/
+mkdir -p $ZSH_THEME_DIR
+ln -sf ~/.dotfiles/zsh/rashiq.zsh-theme $ZSH_THEME_DIR
