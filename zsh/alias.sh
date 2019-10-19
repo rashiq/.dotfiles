92599e3bb0901674012fd28bb1815974a3b02d31
Rashiq <rashiq.z@gmail.com>
2019-10-19 03:31:54 +0200

    Git yeet

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -7,3 +7,12 @@ alias brewfile="subl -n ~/.dotfiles/brew/Brewfile"
 movToGif() {
   ffmpeg -i $1 -pix_fmt rgb24 -r 24 -f gif - | gifsicle --optimize=2 > $2
 }
+
+yeet() {
+  if [ -z "$1" ]
+  then
+    git push origin master
+  else
+    git push $1
+  fi
+}
