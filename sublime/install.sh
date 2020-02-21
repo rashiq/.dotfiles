f3ccd15d44f7dca9f649c9910447d78b0c23f226
Rashiq <rashiq.z@gmail.com>
2020-02-21 13:19:10 +0100

    Reformat sublime config

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -pv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-ln -sf ~/.dotfiles/sublime/User/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -sf ~/.dotfiles/sublime/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
