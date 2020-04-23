af8f68fc8a3b179e057012f56220117d226145b3
Rashiq <rashiq.z@gmail.com>
2020-04-23 20:45:39 +0200

    Export Android path

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,5 +1,7 @@
 export PATH="${HOME}/Library/Python/2.7/bin:$PATH"
 export PATH=${HOME}/go/bin:$PATH
+export PATH=$PATH:/Users/rashiq/Library/Android/sdk/platform-tools
+export PATH=$PATH:/Users/rashiq/Library/Android/sdk/tools
 
 export EDITOR='subl -n -w'
 export PAGER='less'
